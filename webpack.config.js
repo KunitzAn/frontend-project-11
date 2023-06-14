f18e69dd3c072b29e0cba8c96a8049b70d8a252e
Anastasia Kunitskaia <kunitskaiaanastasia@gmail.com>
2023-06-14 10:22:02 +0300

    add css and csss

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,8 @@
 
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const NodePolyfillPlugin = require('node-polyfill-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 const isProduction = process.env.NODE_ENV === 'production';
 
@@ -18,11 +20,41 @@ const config = {
     new HtmlWebpackPlugin({
       template: 'index.html',
     }),
-
+    new MiniCssExtractPlugin({
+      filename: 'main.css',
+    }),
+    new NodePolyfillPlugin(),
     // Add your plugins here
     // Learn more about plugins from https://webpack.js.org/configuration/plugins/
   ],
   module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      { test: /\.css$/, use: ['style-loader', 'css-loader', 'postcss-loader'] },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader', 'postcss-loader'],
+      },
+      {
+        test: /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        use: 'url-loader?limit=10000',
+      },
+      {
+        test: /\.(ttf|eot|svg)(\?[\s\S]+)?$/,
+        use: 'file-loader',
+      },
+    ],
+
+    /*
     rules: [
       {
         test: /\.(js|jsx)$/i,
@@ -36,6 +68,7 @@ const config = {
       // Add your rules for custom modules here
       // Learn more about loaders from https://webpack.js.org/loaders/
     ],
+    */
   },
 };
 
